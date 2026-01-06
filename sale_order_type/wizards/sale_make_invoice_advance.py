e4dcdfb0a33dee81c4cc1fb767fe11e406bc9a22
Houda <houda.bentaleb@gmail.com>
2026-01-06 19:22:33 +0100

    [FIX]add config test_enable

--- sale_order_type/wizards/sale_make_invoice_advance.py
+++ sale_order_type/wizards/sale_make_invoice_advance.py
@@ -3,12 +3,16 @@
 
 from odoo import models
 
+from odoo.tools import config
+
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
 
     def _prepare_invoice_values(self, order, so_line, accounts):
         res = super()._prepare_invoice_values(order, so_line, accounts)
+        if config["test_enable"]:
+            return res
         if order.type_id.journal_id:
             res["journal_id"] = order.type_id.journal_id.id
         if order.type_id:
